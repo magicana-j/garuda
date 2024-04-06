9a3a775cbbc2b84ab73d1f3ab34991b678074c4a
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-04-06 22:34:39 +0900

    Update noto-cjk.sh

--- noto-cjk.sh
+++ noto-cjk.sh
@@ -1,1 +1,5 @@
 sudo pacman -S noto-fonts-cjk noto-fonts-extra noto-fonts-emoji
+echo "XMODIFIERS=@im=fcitx" | sudo tee -a /etc/environment
+echo "GTK_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+echo "QT_IM_MODULE=fcitx" | sudo tee -a /etc/environment
+exec fcitx5 -d
